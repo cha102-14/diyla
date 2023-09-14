6fceba168b47df64003b027c18c5df07a602be52
feal1221 <140091337+feal1221@users.noreply.github.com>
2023-09-14 21:46:17 +0800

    Member (#78)
    * 修改前台filter
    
    * 修改密碼bug 刪除追蹤商品bug 新增前台filter(DIY)
    
    * 微調
    
    * 微調最新消息CSS
    
    * no message
    
    * 新增假資料、修改我的訂單CSS、修改個人通知bug

--- diyla_front/src/main/java/com/cha102/diyla/front/controller/notice/NoticeWS.java
+++ diyla_front/src/main/java/com/cha102/diyla/front/controller/notice/NoticeWS.java
@@ -36,8 +36,7 @@ public class NoticeWS {
         }
 
         //排程器 定時取出redis 送到前端
-        timer = new Timer();
-        timer.schedule(new Task(),5000);
+
     }
 
 
@@ -46,12 +45,14 @@ public class NoticeWS {
 
         Set<Integer> memIds = sessionMap.keySet();
         for (Integer memId : memIds){
-            System.out.println(memId);
+            System.out.println("get"+memId);
             JedisNotice.delJedisNotice(memId);
+            System.out.println("delete");
         }
         System.out.println(message);
-        System.out.println("delete");
 
+        timer = new Timer();
+        timer.schedule(new Task(),5000);
 
     }
     @OnError
@@ -71,15 +72,16 @@ public class NoticeWS {
             for (Session session : memIdSessions){
                 if(session.isOpen()){
                     session.getAsyncRemote().sendText(gson.toJson(data));
+                    System.out.println("send"+data);
                 }
             }
 
 //            onMessage(memIdSession,memId);
             //裡面放onmessage方法
             //裡面放取出redis 丟到前端 onmessage移除redis的值?
-//
+//            onopen連線
 
-
+//一連線先取得ID 取出redis 丟到前端
 
         }
     }
